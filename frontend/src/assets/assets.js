4f27b49cb34fda90476bd4e3cbfb368ec67e76a7
VishnuJha100 <vishnujha100@gmail.com>
2025-01-01 09:16:28 +0530

    first commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,251 @@
+import logo from './TechBox horizontal logo.png'
+import l1 from './laptop1.jpg'
+import l11 from './laptop11.jpg'
+import l12 from './laptop12.jpg'
+import l13 from './laptop13.jpg'
+import l14 from './laptop14.jpg'
+import l15 from './laptop15.jpg'
+import l2 from './l2.jpg'
+import l21 from './l21.jpg'
+import l22 from './l22.jpg'
+import l23 from './l23.jpg'
+import l24 from './l24.jpg'
+import l25 from './l25.jpg'
+import l3 from './l3.jpg'
+import l31 from './l31.jpg'
+import l32 from './l32.jpg'
+import l33 from './l33.jpg'
+import l34 from './l34.jpg'
+import l35 from './l35.jpg'
+import l4 from './l4.jpg'
+import l41 from './l41.jpg'
+import l42 from './l42.jpg'
+import l43 from './l43.jpg'
+import l44 from './l44.jpg'
+import l45 from './l45.jpg'
+import l5 from './l5.jpg'
+import l51 from './l51.jpg'
+import l52 from './l52.jpg'
+import l53 from './l53.jpg'
+import l54 from './l54.jpg'
+import l55 from './l55.jpg'
+import m1 from './m1.webp'
+import m11 from './m11.jpg'
+import m12 from './m12.jpg'
+import m13 from './m13.jpg'
+import m14 from './m14.jpg'
+import m15 from './m15.jpg'
+import m2 from './m2.webp'
+import m21 from './m21.jpg'
+import m22 from './m22.jpg'
+import m23 from './m23.jpg'
+import m24 from './m24.jpg'
+import m25 from './m25.jpg'
+import m3 from './m3.webp'
+import m31 from './m31.jpg'
+import m32 from './m32.jpg'
+import m33 from './m33.jpg'
+import m34 from './m34.jpg'
+import m35 from './m35.jpg'
+import k1 from './k1.jpg'
+import k11 from './k11.jpg'
+import k12 from './k12.jpg'
+import k13 from './k13.jpg'
+import k14 from './k14.jpg'
+import k15 from './k15.jpg'
+import k2 from './k2.jpg'
+import k21 from './k21.jpg'
+import k22 from './k22.jpg'
+import k23 from './k23.jpg'
+import k24 from './k24.jpg'
+import k25 from './k25.jpg'
+import k3 from './k3.jpg'
+import k31 from './k31.jpg'
+import k32 from './k32.jpg'
+import k33 from './k33.jpg'
+import k34 from './k34.jpg'
+import k35 from './k35.jpg'
+import search_icon from './search_icon.png'
+import profile_icon from './user icon.png'
+import shopping_cart_icon from './shopping cart icon.png'
+import menu_icon from './menu icon.png'
+import back_icon from './back icon.png'
+import exchange_icon from './exchange icon.png'
+import return_icon from './return icon.png'
+import customer_support from './customer support.png'
+import cross_icon from './cross icon.png'
+import star_icon from './star icon.png'
+
+export const assets = {
+    logo,
+    search_icon,
+    profile_icon,
+    shopping_cart_icon,
+    menu_icon,
+    back_icon,
+    l1,
+    exchange_icon,
+    return_icon,
+    customer_support,
+    cross_icon,
+    star_icon
+}
+
+export const products = [
+    {
+        _id: "aaaaa",
+        name: "ASUS TUF Gaming A15",
+        description: "AMD Ryzen 7 7435Hs, 15.6-Inch, FHD 144Hz, Gaming Laptop (16GB RAM/512GB SSD/NVIDIA Geforce RTX 3050/Windows 11/48WHR/Graphite Black/2.3 Kg), FA506NCR-HN054W",
+        price: 71990,
+        image: [l1, l11, l12, l13, l14, l15],
+        category: "Laptop",
+        subCategory: "Gaming",
+        bestSeller: true
+    },
+    {
+        _id: "aaaaa",
+        name: "ASUS TUF Gaming A15+",
+        description: "AMD Ryzen 7 7435Hs, 15.6-Inch, FHD 144Hz, Gaming Laptop (16GB RAM/512GB SSD/NVIDIA Geforce RTX 3050/Windows 11/48WHR/Graphite Black/2.3 Kg), FA506NCR-HN054W",
+        price: 61990,
+        image: [l2, l21, l22, l23, l24, l25],
+        category: "Laptop",
+        subCategory: "Gaming",
+        bestSeller: true
+    },
+    {
+        _id: "aaaac",
+        name: "MSI Thin A15",
+        description: "AMD 7th Gen. Ryzen 5 7535HS, 40CM Gaming Laptop(8GB/512GB NVMe SSD/Windows 11 Home/Nvidia GeForce RTX 2050, 4GB GDDR6/Cosmos Gray/1.86Kg), B7UCX-216IN",
+        price: 49890,
+        image: [l3, l31, l32, l33, l34, l35],
+        category: "Laptop",
+        subCategory: "Gaming",
+        bestSeller: true
+    },
+    {
+        _id: "aaaad",
+        name: "MSI Thin 15",
+        description: "Intel 12th Gen. Core i5 12450H, 40CM Gaming Laptop(16GB/1TB NVMe SSD/Windows 11 Home/Nvidia GeForce RTX 3050, 4GB GDDR6/Cosmos Gray/1.86kg), B12UC-2086IN",
+        price: 59990,
+        image: [l4, l41, l42, l43, l44, l45],
+        category: "Laptop",
+        subCategory: "Professional",
+        bestSeller: true
+    },
+    {
+        _id: "aaaae",
+        name: "MSI Raider GE78 HX",
+        description: "Intel 14th Gen. i9-14900HX, 43.18CM QHD+ 240Hz Gaming Laptop (32GB/2TB NVMe SSD/Windows 11 Home/NVIDIA GeForce RTX 4090, GDDR6 16GB/Core Black/3.1Kg), 14VIG-804IN",
+        price: 347290,
+        image: [l5, l51, l52, l53, l54, l55],
+        category: "Laptop",
+        subCategory: "Gaming",
+        bestSeller: true
+    },
+    {
+        _id: "aaaae",
+        name: "MSI Raider GE78 HX",
+        description: "Intel 14th Gen. i9-14900HX, 43.18CM QHD+ 240Hz Gaming Laptop (32GB/2TB NVMe SSD/Windows 11 Home/NVIDIA GeForce RTX 4090, GDDR6 16GB/Core Black/3.1Kg), 14VIG-804IN",
+        price: 347290,
+        image: [l5, l51, l52, l53, l54, l55],
+        category: "Laptop",
+        subCategory: "Premium",
+        bestSeller: true
+    },
+    {
+        _id: "aaaae",
+        name: "MSI Raider GE78 HX",
+        description: "Intel 14th Gen. i9-14900HX, 43.18CM QHD+ 240Hz Gaming Laptop (32GB/2TB NVMe SSD/Windows 11 Home/NVIDIA GeForce RTX 4090, GDDR6 16GB/Core Black/3.1Kg), 14VIG-804IN",
+        price: 347290,
+        image: [l5, l51, l52, l53, l54, l55],
+        category: "Laptop",
+        subCategory: "Gaming",
+        bestSeller: true
+    },
+    {
+        _id: "aaaae",
+        name: "MSI Raider GE78 HX",
+        description: "Intel 14th Gen. i9-14900HX, 43.18CM QHD+ 240Hz Gaming Laptop (32GB/2TB NVMe SSD/Windows 11 Home/NVIDIA GeForce RTX 4090, GDDR6 16GB/Core Black/3.1Kg), 14VIG-804IN",
+        price: 347290,
+        image: [l5, l51, l52, l53, l54, l55],
+        category: "Laptop",
+        subCategory: "Gaming",
+        bestSeller: true
+    },
+    {
+        _id: "aaaae",
+        name: "MSI Raider GE78 HX",
+        description: "Intel 14th Gen. i9-14900HX, 43.18CM QHD+ 240Hz Gaming Laptop (32GB/2TB NVMe SSD/Windows 11 Home/NVIDIA GeForce RTX 4090, GDDR6 16GB/Core Black/3.1Kg), 14VIG-804IN",
+        price: 347290,
+        image: [l5, l51, l52, l53, l54, l55],
+        category: "Laptop",
+        subCategory: "Gaming",
+        bestSeller: true
+    },
+    {
+        _id: "aaaae",
+        name: "MSI Raider GE78 HX",
+        description: "Intel 14th Gen. i9-14900HX, 43.18CM QHD+ 240Hz Gaming Laptop (32GB/2TB NVMe SSD/Windows 11 Home/NVIDIA GeForce RTX 4090, GDDR6 16GB/Core Black/3.1Kg), 14VIG-804IN",
+        price: 347290,
+        image: [l5, l51, l52, l53, l54, l55],
+        category: "Laptop",
+        subCategory: "Gaming",
+        bestSeller: true
+    },
+    {
+        _id: "mmmma",
+        name: "Portronics Special XX",
+        description: "High Performance Wired USB Gaming Mouse, Hero 25K Sensor, 25,600 DPI, RGB, Adjustable Weights, 11 Programmable Buttons, On-Board Memory, PC/Mac - Black",
+        price: 399,
+        image: [m1,m11,m12,m13,m14,m15],
+        category: "Mouse",
+        subCategory: "Gaming",
+    },
+    {
+        _id: "mmmmb",
+        name: "Portronics G PRO X",
+        description: "High Performance Wired USB Gaming Mouse, Hero 25K Sensor, 25,600 DPI, RGB, Adjustable Weights, 11 Programmable Buttons, On-Board Memory, PC/Mac - Black",
+        price: 499,
+        image: [m2,m21,m22,m23,m24,m25],
+        category: "Mouse",
+        subCategory: "Professional",
+    },
+    {
+        _id: "mmmmc",
+        name: "Logitech G502 Hero",
+        description: "High Performance Wired USB Gaming Mouse, Hero 25K Sensor, 25,600 DPI, RGB, Adjustable Weights, 11 Programmable Buttons, On-Board Memory, PC/Mac - Black",
+        price: 3499,
+        image: [m3,m31,m32,m33,m34,m35],
+        category: "Mouse",
+        subCategory: "Premium",
+    },
+    {
+        _id: "kkka",
+        name: "Logitech G502 Hero",
+        description: "High Performance Wired USB Gaming Mouse, Hero 25K Sensor, 25,600 DPI, RGB, Adjustable Weights, 11 Programmable Buttons, On-Board Memory, PC/Mac - Black",
+        price: 1499,
+        image: [k1,k11,k12,k13,k14,k15],
+        category: "Keyboard",
+        subCategory: "Gaming",
+    },
+    {
+        _id: "kkkkb",
+        name: "Logitech G502 Hero",
+        description: "High Performance Wired USB Gaming Mouse, Hero 25K Sensor, 25,600 DPI, RGB, Adjustable Weights, 11 Programmable Buttons, On-Board Memory, PC/Mac - Black",
+        price: 2499,
+        image: [k2,k21,k22,k23,k24,k25],
+        category: "Keyboard",
+        subCategory: "Professional",
+    },
+    {
+        _id: "kkkkc",
+        name: "Logitech G502 Hero",
+        description: "High Performance Wired USB Gaming Mouse, Hero 25K Sensor, 25,600 DPI, RGB, Adjustable Weights, 11 Programmable Buttons, On-Board Memory, PC/Mac - Black",
+        price: 3499,
+        image: [k1,k31,k32,k33,k34,k35],
+        category: "Keyboard",
+        subCategory: "Premium",
+    }
+]
+
+
